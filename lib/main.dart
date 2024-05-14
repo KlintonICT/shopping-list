3951da9ea08398f6c458903f8f0e180e30077a70
KlintonICT <chhunklinton91@gmail.com>
2024-05-14 13:11:25 +0700

    feat: create add item form screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 147, 299, 250),
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
           brightness: Brightness.dark,
           surface: const Color.fromARGB(255, 42, 51, 59),
         ),
